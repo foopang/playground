47cf8d8d3566c8d1c47f29ab684eea8dae25d814
Foo Pang <foo.pang@gmail.com>
2017-05-07 10:38:46 +0800

    Build a CRUD App Using React, Redux and FeathersJS

---
+++ react-contact-manager/src/components/contact-form.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import { Form, Grid, Button } from 'semantic-ui-react';
+import { Field, reduxForm } from 'redux-form';
+import classnames from 'classnames';
+
+class ContactForm extends Component {
+  renderField = ({input, label, type, meta: { touched, error }}) => (
+    <Form.Field className={classnames({error: touched && error})}>
+      <label>{label}</label>
+      <input {...input} placeholder={label} type={type}/>
+      {touched && error && <span className="error">{error.message}</span>}
+    </Form.Field>
+  )
+
+  componentWillReceiveProps = (nextProps) => {
+    const { contact } = nextProps;
+    if (contact._id !== this.props.contact._id) {
+      this.props.initialize(contact);
+    }
+  }
+
+  render() {
+    const { handleSubmit, pristine, submitting, loading } = this.props;
+
+    return (
+      <Grid centered columns={2}>
+        <Grid.Column>
+          <h1 style={{marginTop:"1em"}}>{this.props.contact._id ? 'Edit Contact' : 'Add New Contact'}</h1>
+          <Form onSubmit={handleSubmit} loading={loading}>
+            <Form.Group widths="equal">
+              <Field name="name.first" type="text" component={this.renderField} label="First Name"/>
+              <Field name="name.last" type="text" component={this.renderField} label="Last Name"/>
+            </Form.Group>
+            <Field name="phone" type="text" component={this.renderField} label="Phone"/>
+            <Field name="email" type="text" component={this.renderField} label="Email"/>
+            <Button primary type="submit" disabled={pristine || submitting}>Save</Button>
+          </Form>
+        </Grid.Column>
+      </Grid>
+    )
+  }
+}
+
+const validate = (values) => {
+  const errors = {name: {}};
+  if (!values.name || !values.name.first) {
+    errors.name.first = {
+      message: 'You need to provide First Name'
+    }
+  }
+  if (!values.phone) {
+    errors.phone = {
+      message: 'You need to provide a Phone number'
+    }
+  } else if (!/^\+(?:[0-9] ?){6,14}[0-9]$/.test(values.phone)) {
+    errors.phone = {
+      message: 'Phone number must be in International format'
+    }
+  }
+  if (!values.email) {
+    errors.email = {
+      message: 'You need to provide an Email address'
+    }
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = {
+      message: 'Invalid email address'
+    }
+  }
+  return errors;
+}
+
+export default reduxForm({form: 'contact', validate})(ContactForm);
---
+++ react-contact-manager/src/reducers/index.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+import ContactReducer from './contact-reducer';
+
+const reducers = {
+  contactStore: ContactReducer,
+  form: formReducer
+}
+
+const rootReducer = combineReducers(reducers);
+
+export default rootReducer;
